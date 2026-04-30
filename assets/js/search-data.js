65b8a0eaa2d469b43fef3bc1e741984500ef428d
ahmed-nady <ahmednady56@gmail.com>
2026-04-30 14:52:51 +0000

    Deploying to gh-pages from @ ahmed-nady/ahmed-nady.github.io@233dc58d97d55d2be27b6ac45ff6cffd79f1d540 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -406,6 +406,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/SportsPlayerPerformanceAnalysis/";
+            },},{id: "projects-student-engagement-measurement",
+          title: 'Student Engagement Measurement',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/StudentEngagementMeasurement/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
